5e5e64e3602fc3c0afe19a79257b1f9b4d679c27
Nirmallya07 <bhowmicknirmallya07@gmail.com>
2026-08-07 01:08:03 +0530

    Queue basic code done

--- StackAndQueue/Queue.java
+++ StackAndQueue/Queue.java
@@ -1,5 +1,64 @@
 package StackAndQueue;
 
 public class Queue {
+    private int[] arr;
+    private int last;
+    public Queue(int arrayLength) {
+        arr = new int[arrayLength];
+        last = -1;
+    }
+    private boolean isEmpty() {
+        return last == -1;
+    }
+    private boolean isFull() {
+        return last == arr.length - 1;
+    }
+    public int size() {
+        return last + 1;
+    }
+    private void leftShiftArray() {
+        for (int i = 0; i < last; i++) {
+            arr[i] = arr[i+1];
+        }
+    }
+    public void display() {
+        int n = size();
+        System.out.print("Front--> ");
+        for(int i = 0; i < size(); i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.print(" -->Rear\n");
+    }
+    public void queue(int value) {
+        if (isFull()) throw new IllegalStateException("\"Queue Overflow\"");
+        arr[++last] = value;
+    }
 
+    public int dequeue() {
+        if (isEmpty()) throw new IllegalStateException("\"Queue Underflow\"");
+        int val = arr[0];
+        leftShiftArray();
+        last--;
+        return val;
+    }
+
+    public static void main(String[] args) {
+        Queue queue = new Queue(5);
+        queue.queue(3);
+        queue.queue(4);
+        queue.queue(5);
+        queue.queue(10);
+        queue.queue(2);
+        queue.display();
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+        queue.display();
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+        queue.queue(33);
+        queue.display();
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+    }
 }
